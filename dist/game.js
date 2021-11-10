81b9f40ee7d8b60bb40864d64cfa3b0aeb08c80b
Byron Hambly <bizzle@tari.com>
2021-11-10 22:06:09 +0200

    boost with cooldown, and mouse shoot bullets

--- dist/game.js
+++ dist/game.js
@@ -79460,18 +79460,25 @@
   var jumpsAvailable = 0;
   var jumping = false;
   var jumpLock = false;
+  var leftMouse = false;
+  var rightMouse = false;
+  var boosting = false;
+  var boostAvailable = true;
+  var bullets;
   var Demo = class extends Phaser.Scene {
     constructor() {
       super("demo");
     }
     preload() {
       this.load.image("platform", "assets/platform.png");
+      this.load.image("bullet", "assets/bullet.png");
       this.load.spritesheet("dude", "assets/dude.png", {
         frameWidth: 32,
         frameHeight: 48
       });
     }
     create() {
+      bullets = new Bullets(this);
       platforms = this.physics.add.staticGroup();
       platforms.create(400, 600, "platform").setScale(2).refreshBody();
       player = this.physics.add.sprite(100, 450, "dude");
@@ -79497,17 +79504,46 @@
       keyboard = this.input.keyboard.addKeys("w,a,s,d,f,space,shift");
       console.log("keyboard", keyboard);
       this.physics.add.collider(player, platforms);
+      this.input.mouse.onMouseDown((e) => console.log("mouseDown", e));
+      this.input.mouse.onMouseUp((e) => console.log("mouseUp", e));
     }
     update() {
+      if (this.input.mousePointer.leftButtonDown() && !leftMouse) {
+        leftMouse = true;
+        let toX = this.input.mousePointer.x;
+        let toY = this.input.mousePointer.y;
+        console.log("mouse left", toX, toY);
+        this.time.addEvent({delay: 250, callback: () => leftMouse = false});
+        let x = toX - player.x;
+        let y = toY - player.y;
+        let v = new Phaser.Math.Vector2(x, y).normalize();
+        bullets.fireBullet(player.x, player.y, v);
+      }
+      if (this.input.mousePointer.rightButtonDown() && !rightMouse) {
+        rightMouse = true;
+        let x = this.input.mousePointer.x;
+        let y = this.input.mousePointer.y;
+        console.log("mouse right", x, y);
+        this.time.addEvent({delay: 250, callback: () => rightMouse = false});
+      }
+      if (keyboard.shift.isDown && !boosting && boostAvailable) {
+        boosting = true;
+        boostAvailable = false;
+        this.time.addEvent({delay: 1e3, callback: () => boosting = false});
+        this.time.addEvent({
+          delay: 3e3,
+          callback: () => boostAvailable = true
+        });
+      }
       if (keyboard.d.isDown) {
-        if (keyboard.shift.isDown) {
+        if (boosting) {
           player.setVelocityX(400);
         } else {
           player.setVelocityX(200);
         }
         player.anims.play("right", true);
       } else if (keyboard.a.isDown) {
-        if (keyboard.shift.isDown) {
+        if (boosting) {
           player.setVelocityX(-400);
         } else {
           player.setVelocityX(-200);
@@ -79550,4 +79586,40 @@
     }
   };
   var game = new Phaser.Game(config);
+  var Bullet = class extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y) {
+      super(scene, x, y, "bullet");
+    }
+    fire(fromX, fromY, v) {
+      this.body.reset(fromX, fromY);
+      this.setActive(true);
+      this.setVisible(true);
+      this.setVelocity(300 * v.x, 300 * v.y);
+    }
+    preUpdate(time, delta) {
+      super.preUpdate(time, delta);
+      if (this.y <= -32) {
+        this.setActive(false);
+        this.setVisible(false);
+      }
+    }
+  };
+  var Bullets = class extends Phaser.Physics.Arcade.Group {
+    constructor(scene) {
+      super(scene.physics.world, scene, {allowGravity: false});
+      this.createMultiple({
+        frameQuantity: 10,
+        key: "bullet",
+        active: false,
+        visible: false,
+        classType: Bullet
+      });
+    }
+    fireBullet(fromX, fromY, v) {
+      let bullet = this.getFirstDead(false);
+      if (bullet) {
+        bullet.fire(fromX, fromY, v);
+      }
+    }
+  };
 })();
--- src/game.ts
+++ src/game.ts
@@ -6,6 +6,11 @@ let player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
 let jumpsAvailable = 0;
 let jumping = false;
 let jumpLock = false;
+let leftMouse = false;
+let rightMouse = false;
+let boosting = false;
+let boostAvailable = true;
+let bullets;
 
 export default class Demo extends Phaser.Scene {
   constructor() {
@@ -14,6 +19,7 @@ export default class Demo extends Phaser.Scene {
 
   preload() {
     this.load.image("platform", "assets/platform.png");
+    this.load.image("bullet", "assets/bullet.png");
     this.load.spritesheet("dude", "assets/dude.png", {
       frameWidth: 32,
       frameHeight: 48,
@@ -21,6 +27,8 @@ export default class Demo extends Phaser.Scene {
   }
 
   create() {
+    bullets = new Bullets(this);
+
     platforms = this.physics.add.staticGroup();
     platforms.create(400, 600, "platform").setScale(2).refreshBody();
 
@@ -52,24 +60,48 @@ export default class Demo extends Phaser.Scene {
     keyboard = this.input.keyboard.addKeys("w,a,s,d,f,space,shift");
     console.log("keyboard", keyboard);
     this.physics.add.collider(player, platforms);
+    this.input.mouse.onMouseDown((e) => console.log("mouseDown", e));
+    this.input.mouse.onMouseUp((e) => console.log("mouseUp", e));
   }
 
   update() {
-    // if (this.input.mousePointer.leftButtonDown()) {
-    //   console.log("mouse left");
-    // }
-    // if (this.input.mousePointer.rightButtonDown()) {
-    //   console.log("mouse right");
-    // }
+    if (this.input.mousePointer.leftButtonDown() && !leftMouse) {
+      leftMouse = true;
+      let toX = this.input.mousePointer.x;
+      let toY = this.input.mousePointer.y;
+      console.log("mouse left", toX, toY);
+      this.time.addEvent({ delay: 250, callback: () => (leftMouse = false) });
+      let x = toX - player.x;
+      let y = toY - player.y;
+      let v = new Phaser.Math.Vector2(x, y).normalize();
+      bullets.fireBullet(player.x, player.y, v);
+    }
+    if (this.input.mousePointer.rightButtonDown() && !rightMouse) {
+      rightMouse = true;
+      let x = this.input.mousePointer.x;
+      let y = this.input.mousePointer.y;
+      console.log("mouse right", x, y);
+      this.time.addEvent({ delay: 250, callback: () => (rightMouse = false) });
+    }
+    if (keyboard.shift.isDown && !boosting && boostAvailable) {
+      boosting = true;
+      boostAvailable = false;
+      this.time.addEvent({ delay: 1000, callback: () => (boosting = false) });
+      this.time.addEvent({
+        delay: 3000,
+        callback: () => (boostAvailable = true),
+      });
+    }
+
     if (keyboard.d.isDown) {
-      if (keyboard.shift.isDown) {
+      if (boosting) {
         player.setVelocityX(400);
       } else {
         player.setVelocityX(200);
       }
       player.anims.play("right", true);
     } else if (keyboard.a.isDown) {
-      if (keyboard.shift.isDown) {
+      if (boosting) {
         player.setVelocityX(-400);
       } else {
         player.setVelocityX(-200);
@@ -115,3 +147,49 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
+
+class Bullet extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene, x, y) {
+    super(scene, x, y, "bullet");
+  }
+
+  fire(fromX, fromY, v) {
+    this.body.reset(fromX, fromY);
+
+    this.setActive(true);
+    this.setVisible(true);
+
+    this.setVelocity(300 * v.x, 300 * v.y);
+  }
+
+  preUpdate(time, delta) {
+    super.preUpdate(time, delta);
+
+    if (this.y <= -32) {
+      this.setActive(false);
+      this.setVisible(false);
+    }
+  }
+}
+
+class Bullets extends Phaser.Physics.Arcade.Group {
+  constructor(scene) {
+    super(scene.physics.world, scene, { allowGravity: false });
+
+    this.createMultiple({
+      frameQuantity: 10,
+      key: "bullet",
+      active: false,
+      visible: false,
+      classType: Bullet,
+    });
+  }
+
+  fireBullet(fromX, fromY, v) {
+    let bullet = this.getFirstDead(false);
+
+    if (bullet) {
+      bullet.fire(fromX, fromY, v);
+    }
+  }
+}
